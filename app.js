f34ac6a1b38e15d9d4ad4d6ce2d13a5017b366f8
Fernando Mayorga <mayorgarojasgamedeveloper>
2018-04-26 16:42:51 -0500

    Final de la api

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var cors = require('cors');
 var db = require('./assets/database.js');
 
+var indexRouter = require('./routes/index');
 var usuarioRouter = require('./routes/usuario');
 var adminRouter = require('./routes/administrador');
 var peliculaRouter = require('./routes/pelicula');
@@ -13,7 +14,7 @@ var funcionRouter = require('./routes/funcion');
 var reciboRouter = require('./routes/recibo');
 var logRouter = require('./routes/log');
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 var app = express();
 
 // view engine setup
@@ -27,13 +28,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.send('API Cineperla');
-});
-
+app.use('/', indexRouter);
 app.use('/usuario', usuarioRouter);
 app.use('/admin', adminRouter);
-//app.use('/pelicula', peliculaRouter);
+app.use('/pelicula', peliculaRouter);
 app.use('/funcion', funcionRouter);
 app.use('/recibo', reciboRouter);
 app.use('/log', logRouter);
@@ -41,21 +39,3 @@ app.use('/log', logRouter);
 app.listen(port, function() {
 	console.log('Our app is running on http://localhost:' + port);
 });
-
-// catch 404 and forward to error handler
-/*app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;*/
